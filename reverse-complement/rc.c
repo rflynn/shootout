87869f945b80ed2b4e2185bc6b45461e421d2bb5
pizza <pizza@fedora11.(none)>
2009-08-19 14:34:52 -0400

    my implementation of reverse-complement works, but is no faster than the existing implementation.

--- reverse-complement/rc.c
+++ reverse-complement/rc.c
@@ -1,24 +1,31 @@
 
+#include <assert.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 
-#define BUFSZ   (1024 * 512UL)
+#define BUFSZ   (1024 * 8UL)
+#define BSWAP64 __builtin_bswap64
 
 struct buf {
   char  *str;
   size_t len, alloc;
 };
 
-static void buf_init(struct buf *b)
+static inline void buf_init(struct buf *b)
 {
   b->len = 0;
   b->alloc = BUFSZ;
   b->str = malloc(b->alloc);
 }
 
-static void buf_grow(struct buf *b, size_t len)
+/**
+ * grow buffer (if necessary) to contain >= BUFSZ available space
+ */
+static inline void buf_grow(struct buf *b, size_t len)
 {
   size_t newlen = b->len + len + BUFSZ;
   if (newlen > b->alloc) {
@@ -29,9 +36,13 @@ static void buf_grow(struct buf *b, size_t len)
   }
 }
 
-static inline void revcomp(struct buf *b)
+/**
+ * reverse and complement the buffer contents in-place
+ * one byte at a time
+ */
+static inline void complement(char *str, size_t len)
 {
-  static const unsigned char Rev[256] = {
+  static const char Rev[256] = {
     ['A'] = 'T', ['a'] = 'T',
     ['B'] = 'V', ['b'] = 'V', 
     ['C'] = 'G', ['c'] = 'G',
@@ -39,72 +50,105 @@ static inline void revcomp(struct buf *b)
     ['G'] = 'C', ['g'] = 'C', 
     ['H'] = 'D', ['h'] = 'D',
     ['K'] = 'M', ['k'] = 'M', 
-    ['L'] = 'K', ['l'] = 'K', 
+    ['M'] = 'K', ['m'] = 'K', 
     ['N'] = 'N', ['n'] = 'N',
     ['R'] = 'Y', ['r'] = 'Y', 
     ['S'] = 'S', ['s'] = 'S', 
     ['T'] = 'A', ['t'] = 'A',
     ['V'] = 'B', ['v'] = 'B', 
     ['W'] = 'W', ['w'] = 'W', 
-    ['Y'] = 'R', ['y'] = 'R'
+    ['Y'] = 'R', ['y'] = 'R',
+    ['\n'] = '\n', ['\r'] = '\r'
   };
-  size_t i = 0,
-         len = b->len,
-         halfway = len / 2;
-  char *c = b->str;
-  while (i <= halfway) {
-    c[i] = c[len-1-i];
-    c[len-1-i] = c[i];
-    i++;
+  while (len--)
+    *str = Rev[(unsigned char)*str], str++;
+}
+
+/**
+ * reverse string in-place
+ */
+static inline void reverse(char *str, size_t len)
+{
+  long i = 0,
+       half = len / 2 + (len & 1);
+  for ( ; i <= half - 8; i += 8) {
+    /* 8 octets at a time */
+    int64_t tmp = *(int64_t *)(str+i);
+    *(int64_t *)(str+i) = BSWAP64(*(int64_t *)(str-i+len-8));
+    *(int64_t *)(str-i+len-8) = BSWAP64(tmp);
+  }
+  while (i < half) {
+    char tmp = str[len-i-1];
+    str[len-i-1] = str[i];
+    str[i++] = tmp;
   }
 }
 
-static void dumplines(struct buf *b)
+static inline void dumplines(struct buf *b)
 {
-  const char *curr = b->str;
-  size_t left = b->len;
-  //revcomp(b);
+  size_t left   = b->len,
+         outlen = left + ((left + 59) / 60);
+  char  out[outlen],
+        *wr     = out,
+        *rd     = b->str;
+  /* format 60-char lines to out */
   while (left >= 60) {
-    (void)write(1, curr, 60);
-    fputc('\n', stdout);
-    curr += 60, left -= 60;
+    memcpy(wr, rd, 60);
+    left -= 60, rd += 60, wr += 60;
+    *wr++ = '\n';
   }
-  if (left) {
-    (void)write(1, curr, left);
-    fputc('\n', stdout);
-  }
-  b->len = 0;
+  if (left)
+    memcpy(wr, rd, left), wr[left] = '\n';
+  /* print */
+  fwrite_unlocked(out, 1, outlen, stdout);
+  //free(out);
 }
 
-static void dump(struct buf *b)
+static inline void dump(struct buf *b, bool id)
 {
-  if ('>' == *b->str)
-    fputs(b->str, stdout);
-  if (b->len)
+  if (b->len) {
+    reverse(b->str, b->len);
     dumplines(b);
+  }
+  if (id)
+    fwrite(b->str + b->len, 1,
+           strlen(b->str + b->len), stdout);
 }
 
-/* read line-by-line a redirected FASTA format file from stdin
+/*
+ * act on a line of input
+ */
+static inline char * addline(char *curr, struct buf *b)
+{
+  if ('>' == *curr) {
+    /* header line, dump contents of buffer */
+    dump(b, true);
+    b->len = 0;
+  } else {
+    /* sequence line, append and adjust buffer */
+    size_t len = strlen(curr);
+    if (len && curr[len-1] == '\n')
+      --len;
+    complement(curr, len);
+    b->len += len;
+    buf_grow(b, len);
+  }
+  return b->str + b->len;
+}
+
+/*
+ * read line-by-line a redirected FASTA format file from stdin
  * for each sequence:
- *   write the id, description, and the reverse-complement sequence in FASTA
- *   format to stdout */
+ *   write the id, description, and the reverse-complement sequence
+ *   in FASTA format to stdout
+ */
 int main(void)
 {
   struct buf b;
   buf_init(&b);
   char *curr = b.str;
-  while (fgets(curr, BUFSZ, stdin)) {
-    if ('>' == *curr) {
-      dump(&b);
-    } else {
-      size_t len = strlen(curr);
-      if (len && b.str[len-1] == '\n')
-        --len;
-      b.len += len;
-      curr += len;
-      buf_grow(&b, len);
-    }
-  }
-  dump(&b);
+  while (fgets(curr, BUFSZ, stdin))
+    curr = addline(curr, &b);
+  dump(&b, false);
   return 0; 
 }
