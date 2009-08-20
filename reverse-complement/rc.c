0086352f4b82cf14531af444d557c7ad970c9c2b
pizza <pizza@fedora11.(none)>
2009-08-20 13:32:53 -0400

    checkpoint. refactored and working, equivalent speed. the trick is to figure out if we'll use less RAM and/or gzip to smaller than the original program, in which case we have a chance of being accepted.

--- reverse-complement/rc.c
+++ reverse-complement/rc.c
@@ -2,142 +2,113 @@
  * The Computer Language Benchmarks Game
  * http://shootout.alioth.debian.org
  *
- * contributed by Ryan Flynn
+ * contributed by Bob W
+ * refactored by Ryan Flynn
  */
 
-#define _GNU_SOURCE
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+#include <stddef.h>
 #include <string.h>
 
-#define BUFSZ   8192
+#define LINESZ    60
+#define OUTBUFSZ  8 * 1024
 
-struct buf {
-  size_t len,
-         alloc;
-  char  *str;
+/*
+ *  _ _ _ _ _ _ _ _ _ _
+ * |_|_|_|_|_|_|_|_|_|_|
+ * ^head           <-- ^wr (moves backwards)
+ * |<-- alloc bytes -->|
+ */
+struct revbuf {
+  size_t alloc;
+  char  *head,
+        *wr;
 };
 
-/**
- * grow buffer (if necessary) to contain >= BUFSZ available space
+#define buf_end(b)  ((b)->head + (b)->alloc)
+
+/*
+ * grow buffer or die; adjust members appropriately
  */
-static inline void buf_grow(struct buf *b, size_t len)
+static inline void revbuf_grow(struct revbuf *b)
 {
-  b->len += len;
-  size_t newlen = b->len + BUFSZ;
-  if (newlen > b->alloc) {
-    b->alloc = newlen * 2;
-    b->str = realloc(b->str, b->alloc);
-    if (!b->str)
-      perror("realloc"), exit(1);
-  }
+  const char *old = b->head;
+  const size_t wrlen = buf_end(b) - b->wr;
+  b->alloc *= 2;
+  b->head = realloc(b->head, b->alloc);
+  assert(b->head && "realloc");
+  b->wr = memmove(buf_end(b) - wrlen,
+                  b->head + (b->wr - old), wrlen);
 }
 
-/**
- * reverse and complement the buffer contents in-place
- * one byte at a time
+/*
+ * for each byte in rd
+ *   if has a complement
+ *     decrease b->wr and write complement
  */
-static inline void complement(char *str, size_t len)
+static inline void revcomp(char *rd, struct revbuf *b)
 {
   static const char Rev[256] = {
     ['A'] = 'T', ['a'] = 'T',
-    ['B'] = 'V', ['b'] = 'V', 
+    ['B'] = 'V', ['b'] = 'V',
     ['C'] = 'G', ['c'] = 'G',
-    ['D'] = 'H', ['d'] = 'H', 
-    ['G'] = 'C', ['g'] = 'C', 
+    ['D'] = 'H', ['d'] = 'H',
+    ['G'] = 'C', ['g'] = 'C',
     ['H'] = 'D', ['h'] = 'D',
-    ['K'] = 'M', ['k'] = 'M', 
-    ['M'] = 'K', ['m'] = 'K', 
+    ['K'] = 'M', ['k'] = 'M',
+    ['M'] = 'K', ['m'] = 'K',
     ['N'] = 'N', ['n'] = 'N',
-    ['R'] = 'Y', ['r'] = 'Y', 
-    ['S'] = 'S', ['s'] = 'S', 
+    ['R'] = 'Y', ['r'] = 'Y',
+    ['S'] = 'S', ['s'] = 'S',
     ['T'] = 'A', ['t'] = 'A',
-    ['V'] = 'B', ['v'] = 'B', 
-    ['W'] = 'W', ['w'] = 'W', 
+    ['V'] = 'B', ['v'] = 'B',
+    ['W'] = 'W', ['w'] = 'W',
     ['Y'] = 'R', ['y'] = 'R'
   };
-  for (unsigned i = 0; i < len; i++)
-    str[i] = Rev[(unsigned char)str[i]];
-}
-
-/**
- * reverse string in-place
- */
-static inline void reverse(char *str, size_t len)
-{
-  long i = 0,
-       half = len / 2 + (len & 1);
-  for (i = 0; i <= half - 8; i += 8) {
-    /* 8 bytes at the same time */
-    int64_t tmp = *(int64_t *)(str+i);
-    *(int64_t *)(str+i) = __builtin_bswap64(*(int64_t *)(str-i+len-8));
-    *(int64_t *)(str-i+len-8) = __builtin_bswap64(tmp);
+  while (*rd) {
+    char c = Rev[(unsigned char)(*rd++)];
+    if (c)
+      *--b->wr = c;
   }
-  while (i < half) {
-    char tmp = str[len-i-1];
-    str[len-i-1] = str[i];
-    str[i++] = tmp;
-  }
-}
-
-static inline void dumplines(struct buf *b)
-{
-# define LINE 60
-  size_t outlen = b->len + ((b->len + LINE - 1) / LINE),
-         left   = b->len,
-         done   = left % LINE,
-         wr     = 0,
-         rd     = 0;
-  char  *out    = malloc(outlen);
-  for (rd = wr = 0; left != done;
-       rd += LINE, wr += LINE+1, left -= LINE)
-  {
-    memcpy(out+wr, b->str+rd, LINE);
-    out[wr+LINE] = '\n';
-  }
-  if (left) {
-    memcpy(out+wr, b->str+rd, left);
-    out[wr+left] = '\n';
-  }
-  fwrite_unlocked(out, 1, outlen, stdout);
-  free(out);
-  b->len = 0;
-}
-
-static inline void dump(struct buf *b)
-{
-  reverse(b->str, b->len);
-  dumplines(b);
 }
 
 /*
- * act on a line of input
+ * format and write each line of LINESZ or fewer bytes
  */
-static inline char * addline(char *curr, size_t len, struct buf *b)
+static inline void output(struct revbuf *b)
 {
-  if ('>' != *curr) {
-    complement(curr, len - 1);
-    buf_grow(b, len - 1);
-  } else {
-    dump(b);
-    fwrite_unlocked(curr, 1, len, stdout);
+  char *pq = b->head;
+  while (b->wr < buf_end(b)) {
+    size_t len = LINESZ < buf_end(b) - b->wr ?
+                 LINESZ : buf_end(b) - b->wr;
+    memmove(pq, b->wr, len); /* move l to free space */
+    b->wr += len;
+    pq += len;
+    *pq++ = '\n';
   }
-  return b->str + b->len;
+  fwrite(b->head, 1, pq - b->head, stdout);
 }
 
-/*
- * read line-by-line a redirected FASTA format file from stdin
- * for each sequence:
- *   write the id, description, and the reverse-complement sequence
- *   in FASTA format to stdout
- */
 int main(void)
 {
-  struct buf b = { 0, BUFSZ, malloc(BUFSZ) };
-  char *curr = b.str;
-  while (fgets_unlocked(curr, BUFSZ, stdin))
-    curr = addline(curr, __builtin_strlen(curr), &b);
-  dump(&b);
-  return 0; 
+  struct revbuf b = { OUTBUFSZ, malloc(OUTBUFSZ), 0 };
+  char l[LINESZ+1];
+  char *rd = fgets(l, sizeof l, stdin);
+  
+  assert("Buffer allocation" && b.head);
+  assert("No input data" && rd);
+  assert("First char not '>'" && '>' == *l);
+  b.wr = buf_end(&b);
+  while (rd) {
+    fputs(l, stdout); /* print id */
+    while ((rd = fgets(l, sizeof l, stdin)) && '>' != *l) {
+      if (b.wr <= b.head + sizeof l)
+        revbuf_grow(&b);
+      revcomp(l, &b);
+    }
+    output(&b);
+  }
+  return 0;
 }
